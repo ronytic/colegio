136ff0dc21264c52ac969a58adb04f3a8b7cb92e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-08-30 19:04:50 -0400

    Corregido Idioma

--- notas/cualitativadocente/guardarnota.php
+++ notas/cualitativadocente/guardarnota.php
@@ -8,10 +8,10 @@ $rango1=$_POST['Rango1'];
 $rango2=$_POST['Rango2'];
 $rango3=$_POST['Rango3'];
 $rango4=$_POST['Rango4'];
-$valores=array("PrimerRango"=>"'$rango1'",
-				"SegundoRango"=>"'$rango2'",
-				"TercerRango"=>"'$rango3'",
-				"CuartoRango"=>"'$rango4'",
+$valores=array("PrimerRango"=>"'".mayuscula($rango1)."'",
+				"SegundoRango"=>"'".mayuscula($rango2)."'",
+				"TercerRango"=>"'".mayuscula($rango3)."'",
+				"CuartoRango"=>"'".mayuscula($rango4)."'",
 			);
 $notascualitativa->actualizarRegistro($valores,"CodNotasCualitativa=".$CodNotasCualitativa);
 echo $idioma['NotasGuardadoCorrectamente'];
