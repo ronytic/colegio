90593f2ed8f34938adb270d08d1c2c25e241bbcb
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-10-02 20:54:17 -0400

    Agregado Cargador en Actualización

--- js/seguridad/veraccesos.js
+++ js/seguridad/veraccesos.js
@@ -1,6 +1,7 @@
 $(document).on("ready",function(){
 	$("input[name=Fecha]").datepicker({maxDate:"0 D",changeMonth: true,changeYear: true,dateFormat: 'dd-mm-yy'});
 	$("#ver").click(function(e) {
+		cargandoG('#resultado');
         $.post("ver.php",{'Nivel':$("select[name=Nivel]").val(),"Fecha":$('input[name=Fecha]').val()},function(data){
 			$('#resultado').html(data);	
 		});
