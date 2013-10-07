76318151e6b7bd329dce979dc0991d1cdf26abe2
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-10-06 20:29:34 -0400

    Formulario de Asginacion de Codigo Corregido

--- codigos/asignacioncodigoalumno/formularioasignacion.php
+++ codigos/asignacioncodigoalumno/formularioasignacion.php
@@ -1,5 +1,11 @@
 <?php
 include_once("../../login/check.php");
+include_once("../../class/alumno.php");
+$alumno=new alumno;
+$CodAlumno=$_POST['CodAlumno'];
+$al=$alumno->mostrarTodoDatos($CodAlumno);
+//print_r($al);
+$al=array_shift($al);
 if(!empty($_POST)){
 ?>
 <form action="enviar" id="formuCodigo">
@@ -10,6 +16,8 @@ if(!empty($_POST)){
             <input type="text" name="Codigo" id="Codigo" autofocus placeholder="<?php echo $idioma['CodigoBarra']?>" value=""  class="span12" autocomplete="off"/>
         </div>
     </div>
+    <hr class="separador">
+			<?php echo $idioma['CodigoAsignado']?>: <?php echo $al['CodBarra']?><br>
     <input type="submit" value="<?php echo $idioma['Guardar']?>" class="btn btn-success">
 </form>
 <?php }?>
