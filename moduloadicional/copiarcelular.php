d7ee182a534f8651da1c7a6edc390ce599e156b8
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-10-23 18:17:37 -0400

    Corregido Modulo Inicial

--- moduloadicional/copiarcelular.php
+++ moduloadicional/copiarcelular.php
@@ -7,7 +7,15 @@ $curso=new curso;
 foreach($curso->mostrar() as $cur){//echo $cur['CodCurso']."<br>";
 	foreach($alumno->mostrarAlumnosCurso($cur['CodCurso']) as $al){	
 		if($al['CelularSMS']==""){
-				$valores=array("CelularSMS"=>"'".$al['Celular']."'","ActivarSMS"=>"'1'");
+				if($al['Celular']==""){
+					if($al['CelularP']==""){
+						$valores=array("CelularSMS"=>"'".$al['CelularM']."'","ActivarSMS"=>"'1'");
+					}else{
+						$valores=array("CelularSMS"=>"'".$al['CelularP']."'","ActivarSMS"=>"'1'");	
+					}
+				}else{
+					$valores=array("CelularSMS"=>"'".$al['Celular']."'","ActivarSMS"=>"'1'");
+				}
 				$alumno->actualizarDatosAlumno($valores,$al['CodAlumno']);
 		}
 	}
