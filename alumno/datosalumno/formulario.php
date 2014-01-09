83987171943c02b2ee6652db6ae8062bb687c319
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-01-09 18:36:35 -0400

    Corregido Imprimir Reporte

--- alumno/datosalumno/formulario.php
+++ alumno/datosalumno/formulario.php
@@ -4,7 +4,7 @@ if(isset($_POST)){
 	$CodAlumno=$_POST['CodAlumno'];
 ?>
 <a class="btn" id="reportedatos"><?php echo $idioma['ReporteDatosAlumno']?></a>
-<a class="btn btn-info" id="reporteimpresion"><?php echo $idioma['ReporteDatosImpresion']?></a>
+<a class="btn btn-info" id="reporteimpresion"><?php echo $idioma['ImprimirReporte']?></a>
 <a class="btn btn-success" href="alumno.php?CodAlumno=<?php echo $CodAlumno?>"><?php echo $idioma['ModificarDatos']?></a>
 <hr />
 <div id="reporte"></div>
