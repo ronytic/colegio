4fa321d6ae8808da849fcd4613713b98bc317888
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-01-11 01:40:23 -0400

    Corregio Listado Temporal

--- js/listar/tmplistado.js
+++ js/listar/tmplistado.js
@@ -2,16 +2,18 @@ var CodCurso;
 var CodAlumno;
 $(document).on("ready",inicio);
 function inicio(){
-	buscadorLista($("#icurso"),$("#selectcurso"),0);
+	buscadorLista($("#icurso"),$("#selectcurso"),0,"cuadro");
 //	buscadorLista($("#ialumno"),$("#selectalumno"));
 	
 	$("#selectcurso").change(function(e) {
 		var valor=$(this).val();
         CodCurso=valor;
+		cargandoG("#cargandoralumnos");
 		$.post(folder+"listar/alumnostmp.php",{"CodCurso":CodCurso},alumnos);
     });
 	var valor=$("#selectcurso").val();
 	CodCurso=valor;
+	
 	if(CodAlumno!=""){
 		$.post(folder+"listar/alumnostmp.php",{"CodCurso":CodCurso,"CodAlumno":CodAlumno},alumnos);		
 	}else{
@@ -28,8 +30,11 @@ function inicio(){
 		}
 	});
 }
+
 function alumnos(data){
 	$("#selectalumno").html(data)//.trigger("liszt:updated");
+	$("#cargandoralumnos").html('');
+	//$("html,body").animate({scrollTop:150},750);
 	buscadorLista($("#ialumno"),$("#selectalumno"));
 	$("#ialumno").change();
 	var valor=$("#selectalumno:first").val();
