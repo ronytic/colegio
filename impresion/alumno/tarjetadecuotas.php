a06073606e342eeba7e3a6ed4a7760a48c46601f
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-01-25 11:19:16 -0400

    Modificado Tarjeta de Cuotas

--- impresion/alumno/tarjetadecuotas.php
+++ impresion/alumno/tarjetadecuotas.php
@@ -5,8 +5,8 @@ class pdf extends PPDF{
 	
 }
 if(!empty($_GET)){
-	$borde=0;
-	$aumento=58;
+	$borde=0; 
+	$aumento=61;//estaba en 58 
 	
 	include_once("../../class/alumno.php");
 	include_once("../../class/curso.php");
@@ -75,15 +75,15 @@ if(!empty($_GET)){
 	$pdf->AddPage();
 	$pdf->SetFont('Times','B',13);
 	
-	$pdf->SetXY(105,43+$aumento);
+	$pdf->SetXY(106,43.6+$aumento);
 	$pdf->Cell(0,0,utf8_decode(ucwords($alumno['Paterno']." ".$alumno['Materno'])),$borde);
 	$pdf->SetXY($x,50.5+$aumento);
 	$pdf->Cell(0,0,utf8_decode(ucwords($nombre)),$borde);
-	$pdf->SetXY(97,58+$aumento);
+	$pdf->SetXY(99,57.5+$aumento);
 	$pdf->Cell(0,0,utf8_decode(ucwords($Curso1)),$borde);
-	$pdf->SetXY(103-10,64+$aumento);
+	$pdf->SetXY(104-10,64+$aumento);
 	$pdf->Cell(0,0,utf8_decode(ucwords($curso2)),$borde);
-	$pdf->SetXY(119,82.1+$aumento);
+	$pdf->SetXY(119,83.8+$aumento);
 	$pdf->SetFont('Times','B',15);
 	$pdf->Cell(0,0,date("y"),$borde);
 	
