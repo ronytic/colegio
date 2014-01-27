f05a3a3a79a33ce9f660a5ae87fee3919f6e8aa9
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-01-26 21:11:44 -0400

    Corregido Codigos de Barra por Curso

--- codigos/alumnos/curso.php
+++ codigos/alumnos/curso.php
@@ -17,12 +17,12 @@ $folder="../../";
 <meta charset="utf-8">
 </head>
 <body>
-<div style="text-align:center"><h1><?php echo $idioma['CodigoBarra']?></h1></div>
+<div style="text-align:center"><h1><?php echo $idioma['CodigosBarra']?></h1></div>
 <input type="button" value="<?php echo $idioma['Imprimir']?>" onClick="javascript:window.print();" class="btn btn-info"> <?php echo $idioma['Curso']?>: <?php echo $cur['Nombre']?><hr>
 <?php
 foreach($alumno->mostrarDatosAlumnosWhere("CodCurso=".$CodCurso) as $al){
 	?>
-    <div style="display:inline-block;border:#CCC 1px solid;padding:10px;">
+    <div style="display:inline-block;border:#CCC 1px solid;padding:10px; width:200px; text-align:center">
     <div class="centrar"><?php echo capitalizar($al['Paterno']);?> <?php echo capitalizar($al['Materno']);?> <?php echo capitalizar($al['Nombres']);?></div>
     <img src="../../code/barcode.php?code=<?php echo $al['CodBarra']?>&encoding=ANY" class="span2"  height="100">
     
