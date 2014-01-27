05e23c0e014d7d7fd3505015273813182d91965b
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-01-26 21:11:32 -0400

    Corregido Codigos de Alumno

--- codigos/alumnos/alumnos.php
+++ codigos/alumnos/alumnos.php
@@ -20,7 +20,7 @@ $al=$alumno->mostrarTodoDatos($CodAlumno);
 $al=array_shift($al);
 //foreach($alumno->mostrarDatosAlumnosWhere("CodCurso=".$CodCurso) as $al){
 	?>
-    <div style="display:inline-block;border:#CCC 1px solid;padding:10px;">
+    <div style="display:inline-block;border:#CCC 1px solid;padding:10px; width:250px; text-align:center">
     <div class="centrar"><?php echo capitalizar($al['Paterno']);?> <?php echo capitalizar($al['Materno']);?> <?php echo capitalizar($al['Nombres']);?></div>
     <img src="../../code/barcode.php?code=<?php echo $al['CodBarra']?>&encoding=ANY" class="span2"  height="100">
     
