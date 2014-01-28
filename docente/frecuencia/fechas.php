08ccb999b69fae1b749750a5ec07d98309eacda7
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-01-27 21:28:04 -0400

    Corregido Frecuencia de Acceso Fecha

--- docente/frecuencia/fechas.php
+++ docente/frecuencia/fechas.php
@@ -2,28 +2,28 @@
 include_once("../../login/check.php");
 include_once("../../class/docente.php");
 include_once("../../class/logusuario.php");
-$titulo="Frecuencia de Acceso de Docentes";
+$titulo="NFrecuenciaAccesos";
 $folder="../../";
 $docente=new docente;
 $logusuario=new logusuario;
 $CodDocente=$_GET['Cod'];
 $doc=array_shift($docente->mostrarDocente($CodDocente));
 ?>
-<?php include_once("../../cabecerahtml.php"); ?>
+<?php include_once($folder."cabecerahtml.php"); ?>
 
-<?php include_once("../../cabecera.php"); ?>
+<?php include_once($folder."cabecera.php"); ?>
 <div class="container_12" id="cuerpo">
 	<div class="box-header">
     	<h2><i class="icon-signal"></i><span class="break"></span><?php echo $idioma['FrecuenciaAcceso']?></h2>
     </div>
     <div class="box-content">
-    	Nombre Docente:<strong><?php echo ucfirst($doc['Paterno'])?> <?php echo ucfirst($doc['Materno'])?> <?php echo ucfirst($doc['Nombres'])?></strong>
+    	<?php echo $idioma['NombreDocente']?>:<strong><?php echo ucfirst($doc['Paterno'])?> <?php echo ucfirst($doc['Materno'])?> <?php echo ucfirst($doc['Nombres'])?></strong>
         <a href="./" class="btn btn-info btn-mini"><?php echo $idioma['Volver']?></a>
         <hr>
         <a href="#" class="btn btn-success btn-mini" id="exportarexcel"><?php echo $idioma['ExportarExcel']?></a>
     	<table class="table table-bordered table-condensed table-hover table-striped">
         <thead>
-        	<tr><th>Nº</td><th><?php echo $idioma['Dia']?></th><th><?php echo $idioma['FechaAcceso']?></th><th><?php echo $idioma['HoraAcceso']?></th></tr>
+        	<tr><th>N</td><th><?php echo $idioma['Dia']?></th><th><?php echo $idioma['FechaAcceso']?></th><th><?php echo $idioma['HoraAcceso']?></th></tr>
             </thead>
             <?php
 			$i=0;
@@ -47,4 +47,4 @@ $doc=array_shift($docente->mostrarDocente($CodDocente));
     </div>
     <div class="clear"></div>
 </div>
-<?php include_once("../../pie.php"); ?>
+<?php include_once($folder."pie.php"); ?>
