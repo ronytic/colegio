b0b2080281060fe16bf47314dbb1bd872252b74e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-01-28 21:07:57 -0400

    Corregido imprecion de facturas

--- impresion/factura/listado.php
+++ impresion/factura/listado.php
@@ -7,17 +7,19 @@ $titulo=$idioma["ListadoFacturas"];
 include_once("../pdf.php");	
 class PDF extends PPDF{
 	function Cabecera(){
-		global $idioma;
+		global $idioma,$FechaFacturaInicio,$FechaFacturaFin;
+		$this->CuadroCabecera(30,$idioma['RangoFecha'].":",30,fecha2Str($FechaFacturaInicio)." / ".fecha2Str($FechaFacturaFin));
+		$this->ln();
 		$this->TituloCabecera(10,"N");
-		$this->TituloCabecera(25,$idioma['FechaFactura']);
-		$this->TituloCabecera(20,"N ".$idioma['Factura']);
+		$this->TituloCabecera(25,$idioma['Fecha']);
+		$this->TituloCabecera(20,"Nº ".$idioma['Factura']);
 		$this->TituloCabecera(25,$idioma['Nit']);
-		$this->TituloCabecera(40,$idioma['FacturaA']);
+		$this->TituloCabecera(50,$idioma['FacturaA']);
 		$this->TituloCabecera(20,$idioma['TotalBs']);
 		$this->TituloCabecera(20,$idioma['Cancelado']);
 		$this->TituloCabecera(20,$idioma['Cambio']);
 		$this->TituloCabecera(20,$idioma['Estado']);
-		$this->TituloCabecera(50,$idioma['CodigoControl']);
+		$this->TituloCabecera(35,$idioma['CodigoControl']);
 	}
 }
 extract($_GET);
@@ -40,6 +42,9 @@ if($Factura!=""){
 if($Estado!=""){
 	array_push($condi,"Estado LIKE '%$Estado%'");
 }
+if($Tipo!=""){
+	array_push($condi,"Tipo LIKE '%$Tipo%'");
+}
 $where=implode(" and ",$condi);
 include_once("../../class/factura.php");
 $factura=new factura;
@@ -56,16 +61,16 @@ if(count($fac)){
 		$pdf->CuadroCuerpo(25,fecha2Str($f['FechaFactura']),$na,"C",0);
 		$pdf->CuadroCuerpo(20,$f['NFactura'],$na,"R",0);
 		$pdf->CuadroCuerpo(25,$f['Nit'],$na,"R",0);
-		$pdf->CuadroCuerpo(40,$f['Factura'],$na,"",0);
+		$pdf->CuadroCuerpo(50,capitalizar($f['Factura']),$na,"",0);
 		$pdf->CuadroCuerpo(20,number_format($f['TotalBs'],2),$na,"R",1);
 		$pdf->CuadroCuerpo(20,number_format($f['Cancelado'],2),$na,"R",1);
 		$pdf->CuadroCuerpo(20,number_format($f['MontoDevuelto'],2),$na,"R",1);
 		$pdf->CuadroCuerpo(20,$f['Estado'],$na,"",0);
-		$pdf->CuadroCuerpo(50,$f['CodigoControl'],$na,"",0);
+		$pdf->CuadroCuerpo(35,$f['CodigoControl'],$na,"",0);
 		$pdf->Ln();
 	}
 	$pdf->Linea();
-	$pdf->CuadroCuerpo(120,$idioma['Totales'],0,"R",0);
+	$pdf->CuadroCuerpo(130,$idioma['Totales'],0,"R",0);
 	$pdf->CuadroCuerpo(20,number_format($tot,2),1,"R",1);
 	$pdf->CuadroCuerpo(20,number_format($can,2),1,"R",1);
 	$pdf->CuadroCuerpo(20,number_format($cambio,2),1,"R",1);
