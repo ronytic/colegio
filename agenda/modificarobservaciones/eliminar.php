86c79d3b6d25e0d3e1453c0f17982dedc9e849d7
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-04-16 16:59:41 -0400

    Corregido Modificar Observaciones

--- agenda/modificarobservaciones/eliminar.php
+++ agenda/modificarobservaciones/eliminar.php
@@ -1,6 +1,6 @@
 <?php
 include_once("../../login/check.php");
-if(!empty($_POST['CodMateria'])){
+if(!empty($_POST['CodObservacion'])){
 	include_once("../../class/observaciones.php");
 	$observaciones=new observaciones;
 	$CodObservacion=$_POST['CodObservacion'];
