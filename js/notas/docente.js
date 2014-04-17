997cd2c6c0d48aa24718f59ce4fe2eebf8ac66c2
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-04-16 22:19:52 -0400

    Corregido Notas Docente

--- js/notas/docente.js
+++ js/notas/docente.js
@@ -153,12 +153,10 @@ function enviarNota(e){
 function evaluarNota(data){
 	var NotaAprobacion=parseInt($("input[name=NotaAprobacion]").val());
 	if(data.TipoNota=="avanzado"){
-		$('.al_'+data.CodAlumno+'_6').val(data.PD1);
-		$('.al_'+data.CodAlumno+'_9').val(data.P1);
-		$('.al_'+data.CodAlumno+'_12').val(data.P2);
-		$('.al_'+data.CodAlumno+'_13').val(data.PD2);
-		$('.al_'+data.CodAlumno+'_16').val(data.PD3);
-		$('.al_'+data.CodAlumno+'_19').val(data.PD4);
+		$('.al_'+data.CodAlumno+'_4').val(data.PD1);
+		$('.al_'+data.CodAlumno+'_10').val(data.PD2);
+		$('.al_'+data.CodAlumno+'_15').val(data.PD3);
+		$('.al_'+data.CodAlumno+'_20').val(data.PD4);
 	}
 	$('#resultado'+data.CodAlumno).val(data.Resultado);
 	if(data.Dps!=0){$('#dps'+data.CodAlumno).val(data.Dps);}
