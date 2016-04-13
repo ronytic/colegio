fcd5346d7dcca464d60e3fe9303ec3c19a00f4b9
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-04-13 08:30:52 -0400

    Registro de Notas

--- notas/docente/listaalumno.php
+++ notas/docente/listaalumno.php
@@ -142,15 +142,27 @@ if(!empty($_POST)){
 			}else{
 				$amarillo='';
 			}
-			if($i==4 || $i==10 || $i==15 || $i==20){
+            
+            if($casillas['TipoNota']==""){
+               if( $i==1 || $i==3){
+				$amarillo='amarillo';
+                }else{
+                    $amarillo='';
+                } 
+                if($i==2 ||  $i==4){
+				    $verde='verde';
+                }else{
+                    $verde='';
+                }    
+            }
+			if(($i==4 || $i==10 || $i==15 || $i==20) & $casillas['TipoNota']=="avanzado"){
 				$lectura='readonly="readonly"';
 				$tabindex="";
 			}else{
 				$lectura='';
-				$verde='';
+				//$verde='';
 				$numero++;
-				$tabindex='tabindex="'.$numero.'"';
-				
+				$tabindex='tabindex="'.$numero.'"';	
 			}
 			?>
             <td style="text-align:center" class="<?php echo $verde." ".$amarillo?>">
